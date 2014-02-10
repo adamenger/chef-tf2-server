dcf58e2979b27610e20883cc147cce6a834abfd7
Adam Enger <adamenger@gmail.com>
2014-02-10 17:08:20 -0600

    adding in locked in versions of dependency cookbooks

--- metadata.rb
+++ metadata.rb
@@ -1,9 +1,12 @@
 name             'tf2-server'
-maintainer       'YOUR_COMPANY_NAME'
-maintainer_email 'YOUR_EMAIL'
+maintainer       'WTF Enterprises'
+maintainer_email 'adamenger@gmail.com'
 license          'All rights reserved'
 description      'Installs/Configures tf2-server'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          '0.1.0'
 
 depends "build-essential"
+depends "apt", "= 2.3.6"
+depends "yum", "= 2.4.4"
+depends "runit", "= 1.5.8"
